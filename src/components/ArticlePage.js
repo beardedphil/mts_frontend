42efd587dfa0a153a1e52f41cccba955a09cb329
Phil King <phil@beardedphil.com>
2020-12-31 14:44:37 -0600

    Added debounced search functionality. Tested that articles are passed to ArticleList when they are returned from a mock API, and that no articles are passed when the mock API passes back an empty list. Also tested that the API is called with properly formatted keyword data when zero, one, or multiple words are typed into the search field.

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -3,25 +3,49 @@ import axios from 'axios';
 import ArticleList from './ArticleList/ArticleList';
 
 const ArticlePage = () => {
+    const [terms, setTerms] = useState('');
     const [articles, setArticles] = useState();
+    const [keywords, setKeywords] = useState([])
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setKeywords(terms);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [terms]);
 
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('http://localhost:8000/articles/', {
                 params: {
-
+                    keywords
                 }
             });
 
             setArticles(data.results);
         };
         search().then();
-    }, []);
+    }, [keywords]);
 
 
     return (
         <div className="article-page">
             <h1>ArticlePage Component</h1>
+            <div className="form">
+                <div className="field">
+                    <label htmlFor="search">Search</label>
+                    <input
+                        id="search"
+                        onChange={e => setTerms(e.target.value)}
+                        type="text"
+                        value={terms}
+                    />
+                </div>
+            </div>
+
             <ArticleList articles={ articles } />
         </div>
     );
--- src/tests/components/ArticlePage.test.js
+++ src/tests/components/ArticlePage.test.js
@@ -3,53 +3,163 @@ import axios from 'axios'
 import articles from '../fixtures/articles';
 import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
+import { render, fireEvent } from '@testing-library/react';
 import ArticlePage from '../../components/ArticlePage';
 import ArticleList from '../../components/ArticleList/ArticleList';
 import Article from '../../components/Article/Article';
 
 jest.mock('axios');
 
-test('should render ArticlePage correctly', (done) => {
-    // This test passes when the assertions are true, and fails when I change the assertions
-    // to make them false. It is still throwing a console error (...An update to ArticlePage
-    // inside a test was not wrapped in act(...)
+describe('should render ArticlePage correctly', () => {
+    // These tests pass when the assertions are true, and fail when I change the assertions
+    // to make them false. They still throw a console error (...An update to ArticlePage
+    // inside a test was not wrapped in act(...))
     //
     // Wrapping the updates in act(...) has not resolved the error. I've added a card to the
     // backlog to look into this at a later time, however the test is providing the correct
     // output, so fixing this is not a priority.
 
-    const promise = new Promise((resolve, reject) =>
-        setTimeout(
-            () =>
-                resolve({
-                    data: {
-                        results: articles
-                    },
-                }),
-            100
-        )
-    );
+    test('should render ArticlePage correctly when articles are returned', (done) => {
+        const promise = new Promise((resolve, reject) =>
+            setTimeout(
+                () =>
+                    resolve({
+                        data: {
+                            results: articles
+                        },
+                    }),
+                100
+            )
+        );
 
-    axios.get = jest.fn(() => promise);
+        axios.get = jest.fn(() => promise);
 
-    let wrapper = null;
+        let wrapper = null;
 
-    act(() => {
-        wrapper = mount(<ArticlePage />);
+        act(() => {
+            wrapper = mount(<ArticlePage />);
+        });
+
+
+        expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/',
+            {
+                "params": {
+                    "keywords": []
+                }
+            }
+        );
+
+        promise.then(() => {
+            setImmediate(() => {
+                act(() => {
+                    wrapper.update();
+                });
+                expect(wrapper.find(ArticleList).length).toEqual(1);
+                expect(wrapper.find(Article).length).toEqual(articles.length);
+                axios.get.mockClear();
+                done();
+            });
+        });
     });
 
+    test('should render ArticlePage correctly when articles are not returned', (done) => {
+        const promise = new Promise((resolve, reject) =>
+            setTimeout(
+                () =>
+                    resolve({
+                        data: {
+                            results: []
+                        },
+                    }),
+                100
+            )
+        );
 
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/', {"params": {}});
+        axios.get = jest.fn(() => promise);
 
-    promise.then(() => {
-        setImmediate(() => {
-            act(() => {
-                wrapper.update();
+        let wrapper = null;
+
+        act(() => {
+            wrapper = mount(<ArticlePage />);
+        });
+
+
+        expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/',
+            {
+                "params": {
+                    "keywords": []
+                }
+            }
+        );
+
+        promise.then(() => {
+            setImmediate(() => {
+                act(() => {
+                    wrapper.update();
+                });
+                expect(wrapper.find(ArticleList).length).toEqual(1);
+                expect(wrapper.find(Article).length).toEqual(0);
+                axios.get.mockClear();
+                done();
             });
-            expect(wrapper.find(ArticleList).length).toEqual(1);
-            expect(wrapper.find(Article).length).toEqual(articles.length);
-            axios.get.mockClear();
-            done();
         });
     });
 });
+
+
+describe('should call API', () => {
+    let input;
+
+    beforeAll(() => {
+        axios.get = jest.fn();
+
+        const { getByLabelText } = render(<ArticlePage/>);
+        input = getByLabelText('Search');
+    });
+
+    test('with empty array when search bar is empty',async () => {
+        fireEvent.change(input, { target: { value: '' } });
+
+        // Setting a timeout manually to account for search term debouncing
+        setTimeout(() => {
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/',
+                {
+                    "params": {
+                        "keywords": []
+                    }
+                }
+            );
+        }, 600)
+    });
+
+    test('with one element array when search bar has one term',async () => {
+        fireEvent.change(input, { target: { value: 'wonder' } });
+
+        // Setting a timeout manually to account for search term debouncing
+        setTimeout(() => {
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/',
+                {
+                    "params": {
+                        "keywords": [ 'wonder' ]
+                    }
+                }
+            );
+        }, 600)
+    });
+
+    test('with multiple element array when search bar has multiple terms',async () => {
+        fireEvent.change(input, { target: { value: 'wonder woman netflix' } });
+
+        // Setting a timeout manually to account for search term debouncing
+        setTimeout(() => {
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:8000/articles/',
+                {
+                    "params": {
+                        "keywords": [ 'wonder', 'woman', 'netflix' ]
+                    }
+                }
+            );
+        }, 600)
+    });
+});
+
